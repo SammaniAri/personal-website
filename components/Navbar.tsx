213e9ca3f069b0fbcb256047f43b6f219a8fac9a
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-05-10 09:46:36 +0200

    removing dark mode button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,8 +32,8 @@ const NAV_ITEMS: Array<NavItem>=[
 
 const Navbar = () => {
 	//controle Whether the mobile view is open or not
-	const {systemTheme, theme, setTheme}=useTheme()
-	const currentTheme = theme === "system" ? systemTheme : theme
+	//const {systemTheme, theme, setTheme}=useTheme()
+	//const currentTheme = theme === "system" ? systemTheme : theme
 	const [navbar, setNavbar] = useState(false)
 	return (
 		 <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600 ">
@@ -44,7 +44,7 @@ const Navbar = () => {
 				return <a key={idx}>{item.label} </a>
 			})}
 
-			{currentTheme === "dark" ? ( <button onClick={()=>setTheme("light")} className="bg-slate-100 p-2 rounded-xl"> <RiSunLine size={25} color= "black"/> </button>): (<button onClick={()=>setTheme("dark")} className="bg-slate-100 p-2 rounded-xl" > <RiMoonFill size={25}  /> </button>)}
+			 
 			</div>	
 		 
 			</div>
