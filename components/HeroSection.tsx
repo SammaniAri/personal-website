eefae3170b1171891d371ecb511b6dde210ef11d
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-05-10 22:28:03 +0200

    Adjusted the positions of elements in hero section. Added an arrow button.

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,12 +2,13 @@
 "use client";
 import React from "react";
 import { Link } from "react-scroll/modules";
+import { HiArrowDown } from "react-icons/hi";
 
 const HeroSection = () => {
 	return (
 		<section id="Home">
-			<div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
-				<div className=" ">
+			<div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-8 md:text-left md:py-52">
+				<div className="md:w-1/2 md:mt-2">
 					<img
 						className="rounded-full shadow-2xl"
 						src="/bike.jpg"
@@ -16,15 +17,18 @@ const HeroSection = () => {
 						height={400}
 					/>
 				</div>
-				<div>
+				<div className="md:w-3/5 md:mt-2 text-lime-950">
 					<h1 className="text-4xl mt-6 md:mt-0 md:text-7xl">
 						Hej!
 					</h1>
 					<p className="text-lg mt-4 mb-6 md:text-2xl">
-						I'm Sammani, a keen
-						future-Frontend Developer. I
-						like to travel and try out
-						new food recipies when i'm
+						I'm Sammani,
+						<span className="font-semibold text-lime-800">
+							a keen future-Frontend
+							Developer.{" "}
+						</span>{" "}
+						I like to travel and try out
+						new food recipies when I'm
 						not coding.{" "}
 					</p>
 					<Link
@@ -39,6 +43,17 @@ const HeroSection = () => {
 						Projects
 					</Link>
 				</div>
+			</div>
+			<div className="flex flex-row justify-center animate-bounce">
+				<Link
+					to="about"
+					activeClass="active"
+					spy={true}
+					smooth={true}
+					offset={-100}
+					duration={500}>
+					<HiArrowDown size={40} />
+				</Link>
 			</div>
 		</section>
 	);
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
 	const [navbar, setNavbar] =
 		useState(false);
 	return (
-		<header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600 ">
+		<header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600 text-lime-950">
 			<div className="justify-between md:items-center md:flex">
 				<div className="flex items-center justify-between py-3">
 					<div className="md:py-5 md:block">
