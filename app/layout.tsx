90a2d3bccf0f97230df40a5200123508db6bf1f1
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-05-11 09:51:20 +0200

    Added footer to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,34 @@
-"use client"
-import '../styles/globals.css'
-import Navbar from '@/components/Navbar'
-import {ThemeProvider} from "next-themes"
+/** @format */
+
+"use client";
+import "../styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
+	return (
+		<html lang="en">
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body> <ThemeProvider enableSystem = {true} attribute="class"> <Navbar/>{children}</ThemeProvider></body>
-    </html>
-  )
+			<head />
+			<body>
+				{" "}
+				<ThemeProvider
+					enableSystem={true}
+					attribute="class">
+					{" "}
+					<Navbar />
+					{children}
+					<Footer />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
