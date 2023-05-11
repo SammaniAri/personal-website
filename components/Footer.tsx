f054264101953097959c9ac6b294fc9259b5e994
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-05-11 10:14:26 +0200

    Added social media icons in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,55 @@
 
 "use strict";
 import React from "react";
+import {
+	AiOutlineGithub,
+	AiOutlineTwitter,
+	AiOutlineLinkedin,
+	AiOutlineYoutube,
+	AiOutlineInstagram,
+} from "react-icons/ai";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+			<hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
+			<div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
+				<div>
+					{" "}
+					© 2023 Sammani Ariyarathne
+				</div>
+				<div className="flex flex-row items-center justify-center space-x-2 mb-1">
+					<a
+						href="https://github.com/SammaniAri"
+						rel="noreferrer"
+						target="_blank">
+						<AiOutlineGithub
+							className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+							size={30}
+						/>
+					</a>
+					<a
+						href="https://www.instagram.com/sammani_simonette/"
+						rel="noreferrer"
+						target="_blank">
+						<AiOutlineInstagram
+							className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+							size={30}
+						/>
+					</a>
+					<a
+						href="https://www.linkedin.com/in/sammani-ariyarathne/"
+						rel="noreferrer"
+						target="_blank">
+						<AiOutlineLinkedin
+							className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+							size={30}
+						/>
+					</a>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
