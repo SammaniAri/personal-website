d9cc1d5ed79bf24881ca8a0acf6685803fe83921
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-05-11 11:59:08 +0200

    added images to projects

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -14,7 +14,7 @@ const projects = [
 		name: "A single page web app",
 		description:
 			"Me and my team developed a single-page web application as a part of a React course at the vocational school, using a Figma prototype as a basis. This project enabled me to acquire key competencies in React and Next.js, while paying attention to search engine optimization,responsiveness and acccebility achieving the learning outcomes of the course.",
-		image: "/photo.jpg",
+		image: "/singlepage.jpg",
 		github:
 			"https://github.com/SammaniAri/react-app-g8",
 		link: "https://monakinft.vercel.app/",
@@ -23,7 +23,7 @@ const projects = [
 		name: "A Simple Game inspired by Game of Thrones",
 		description:
 			"I developed an interactive game using a public API that allows users to guess Game of Thrones characters within a given time limit. Utilized Firebase to store the scores after completion and displayed a leaderboard at the end of the game. Demonstrated  skills in API integration, game design, and data management through this project.",
-		image: "/photo.jpg",
+		image: "/api.jpg",
 		github:
 			"https://github.com/SammaniAri/react-app-g8",
 		link: "https://game-of-thrones-character-guessing.netlify.app/",
@@ -32,7 +32,7 @@ const projects = [
 		name: " To-do App",
 		description:
 			"Lorem ipsum dolor sit amet. Sed voluptates quia sed officia sint nam exercitationem velit est tempora reiciendis sed corrupti sunt ut eveniet repudiandae ut delectus internos. Aut quos architecto et vero facere et totam provident sit illum velit in perspiciatis autem et corrupti placeat et impedit illo?",
-		image: "/photo.jpg",
+		image: "/todo.jpg",
 		github:
 			"https://github.com/orgs/project-on-data-interaction/repositories",
 		link: " ",
