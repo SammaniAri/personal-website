29119ecb1bd9d54530a22cd5c275fd3813793766
Sammani Ariyarathne <sammani.ari@gmail.com>
2023-06-08 17:34:53 +0200

    Added intersection observer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,38 @@
 /** @format */
 
-"use client";
+"use strict";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import Footer from "@/components/Footer";
 import ProjectSection from "@/components/ProjectSection";
 
 export default function Home() {
+	if (typeof document !== "undefined") {
+		const headingAnimations =
+			document.querySelectorAll(
+				".hidden"
+			);
+		const observer =
+			new IntersectionObserver(
+				(entries) => {
+					entries.forEach((entry) => {
+						entry.target.classList.toggle(
+							"show",
+							entry.isIntersecting
+						);
+					});
+				}
+			);
+
+		headingAnimations.forEach(
+			(headingAnimation) => {
+				observer.observe(
+					headingAnimation
+				);
+			}
+		);
+	}
+
 	return (
 		<main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
 			<HeroSection />
