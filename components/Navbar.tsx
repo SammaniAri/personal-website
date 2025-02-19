b280b7a95f548d1a64a317d386084f94d7e43173
Sammani Ariyarathne <sammani.ari@gmail.com>
2025-02-19 18:19:32 +0100

    aria label to the hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,60 +43,48 @@ const Navbar = () => {
 	const [navbar, setNavbar] =
 		useState(false);
 	return (
-		<header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600 text-lime-950">
-			<div className="justify-between md:items-center md:flex">
-				<div className="flex items-center justify-between py-3">
-					<div className="md:py-5 md:block">
-						<h2 className="text-2xl font-bold">
-							{" "}
-							Sammani Ariyarathne
-						</h2>
-					</div>
-					<div className="md:hidden">
-						<button
-							onClick={() =>
-								setNavbar(!navbar)
-							}>
-							{navbar ? (
-								<IoMdClose size={30} />
-							) : (
-								<IoMdMenu size={30} />
-							)}
-						</button>
-					</div>
-				</div>
-				<div
-					className={`"flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-						navbar ? "block" : "hidden"
-					}`}>
-					<div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-						{NAV_ITEMS.map(
-							(item, idx) => {
-								return (
-									<Link
-										key={idx}
-										to={item.page}
-										className={
-											"block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
-										}
-										activeClass="active"
-										spy={true}
-										smooth={true}
-										offset={-100}
-										duration={500}
-										onClick={() =>
-											setNavbar(!navbar)
-										}>
-										{item.label}
-									</Link>
-								);
-							}
-						)}
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+    <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600 text-lime-950">
+      <div className="justify-between md:items-center md:flex">
+        <div className="flex items-center justify-between py-3">
+          <div className="md:py-5 md:block">
+            <h2 className="text-2xl font-bold"> Sammani Ariyarathne</h2>
+          </div>
+          <div className="md:hidden">
+            <button
+              aria-label={navbar ? "Close menu" : "Open menu"}
+              onClick={() => setNavbar(!navbar)}>
+              {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+            </button>
+          </div>
+        </div>
+        <div
+          className={`"flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+            navbar ? "block" : "hidden"
+          }`}>
+          <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            {NAV_ITEMS.map((item, idx) => {
+              return (
+                <Link
+                  key={idx}
+                  to={item.page}
+                  className={
+                    "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                  }
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  onClick={() => setNavbar(!navbar)}>
+                  {item.label}
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
